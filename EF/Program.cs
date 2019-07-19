fbdb80e9fb351cb6ef4663c3a67f207de6118de3
jmf0991 <jmf0991@gmail.com>
2019-07-19 11:47:34 +0200

    code for creating basic local db and adding an item to table

--- EF/Program.cs
+++ EF/Program.cs
@@ -8,8 +8,15 @@ namespace EF
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            using (var db = new MovieDBContext())
+            {
+                Movie movie = new Movie { Rank = 1, Title = "The Shawshank Redemption", Year = 1994 };
+                db.Movies.Add(movie);
+                db.SaveChanges();
+            }
         }
     }
 }
