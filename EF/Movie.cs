bd0c898ea017f1fbafe1388f6699971a27d88885
jmf0991 <jmf0991@gmail.com>
2019-07-19 11:35:27 +0200

    project set up

---
+++ EF/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Movie
+{
+    public int MovieID { get; set; }
+    public int Rank { get; set; }
+    public string Title { get; set; }
+    public int Year { get; set; }
+}
---
+++ EF/MovieDBContext.cs
@@ -0,0 +1,6 @@
+﻿using System.Data.Entity;
+
+public class MovieDBContext : DbContext
+{
+    public DbSet<Movie> Movies { get; set; }
+}
